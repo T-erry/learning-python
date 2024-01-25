c3afd6a7f3567ff71bd30713ca05a76cbccd4e71
T-erry <terrywachuka10@gmail.com>
2024-01-25 16:41:48 +0300

    covered: lists and multi-dimensional lists

---
+++ 2D_lists.py
@@ -0,0 +1,9 @@
+# 2D lists or multi-dimensional lists = a list of lists
+
+drinks =["coffee", "soda", "tea"]
+dinner =["pizza", "sushi", "hotdog"]
+dessert = ["Cupcakes", "Cookies", "Cheesecakes"]
+
+food = [drinks, dinner, dessert]
+
+print(food[2][1])
---
+++ lists.py
@@ -0,0 +1,29 @@
+# list is an ordered collection 0f similar or different types of items. 
+# in short arrays []
+
+food = ["pizza", "hotdog", "spaghetti"]
+food[0] = "rice"
+# print(food[0])
+
+
+#Used for adding elements to the end of the List. 
+# food.append("icecream")
+
+#Removes a given object from the List. 
+#food.remove("rice")
+
+#Removes and returns the last value from the List
+# food.pop()
+
+# Inserts a given element at a given index in a list. 
+# food.insert(0, "cake")
+
+#Sort a List in ascending, descending, 
+# food.sort()
+
+# This method is used for removing all items from the list. 
+# food.clear()
+
+
+# for i in food:
+#     print(i)
