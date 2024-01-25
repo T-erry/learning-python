616581f6d99526020ca22e90e7eac9adee03cabe
T-erry <terrywachuka10@gmail.com>
2024-01-25 16:43:36 +0300

    covered: set data type

---
+++ sets.py
@@ -0,0 +1,32 @@
+# set = unordered collection and unindexed.
+        # No duplicate value
+
+
+utensils ={"fork", "spoon", "knife"}
+dishes ={"bowl", "cup", "plate", "knife"}
+
+#Adds a given element to a set
+# utensils.add("plate")
+
+#Removes all elements from the set
+#utensils.clear()
+
+ # Removes the element from the set
+# utensils.remove("fork")
+
+#Adds elements to the set
+# utensils.update(dishes)
+
+#Returns a set that has the union of all sets
+#dinner_table = utensils.union(dishes)
+
+#Returns a set that is the difference between two sets
+# what exist in dishes that utensils don't have
+# print(dishes.difference(utensils))
+
+#Returns a set that has the intersection
+# elements in sets that are common
+# print(utensils.intersection(dishes))
+
+# for i in dinner_table:
+#     print(i)
