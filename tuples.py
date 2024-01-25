18f2a86e348f28da70cb252878217dc67c502340
T-erry <terrywachuka10@gmail.com>
2024-01-25 16:42:35 +0300

    covered: tuple data type

---
+++ tuples.py
@@ -0,0 +1,14 @@
+#tuple = ordered collection which is immutable
+        # used to group together related data
+
+student = ("Terry", 21, "female")
+
+#The count() method of Tuple returns the number of times the given element appears in the tuple.
+# print(student.count("Terry"))
+
+#The Index() method returns the first occurrence of the given element from the tuple.
+# print(student.index(21))
+
+# for i in student:
+#     print(i)
+
