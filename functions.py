2b4f6a9da34db365e96575ec4f221fe5d2d29c0d
T-erry <terrywachuka10@gmail.com>
2024-01-25 16:45:23 +0300

    covered: index operator and functions

---
+++ functions.py
@@ -0,0 +1,21 @@
+
+# def add(num1: int, num2: int) -> int:
+# 	"""Add two numbers"""
+# 	num3 = num1 + num2
+
+# 	return num3
+
+#  Driver code
+# num1, num2 = 5, 15
+# ans = add(num1, num2)
+# print(f"The addition of {num1} and {num2} results {ans}.")
+
+# def evenOdd(x):
+#     if (x % 2 == 0):
+#         print("even")
+#     else:
+#         print("odd")
+
+
+# evenOdd(2)
+# evenOdd(3)
---
+++ indexing.py
@@ -0,0 +1,13 @@
+# index operator[] = give access to a sequence's element (str, list, tuples)
+
+name= "terry wachuka!"
+# if(name[0].islower):
+#     name = name.capitalize()
+
+
+first_name = name[0:5].upper()
+last_name = name[5:].lower()
+last_character = name[-1]
+
+
+# print(last_character)
