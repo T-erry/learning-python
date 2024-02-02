1259552737a21790f7223d2cbff4f3ab50f47d00
T-erry <terrywachuka10@gmail.com>
2024-02-02 16:19:13 +0300

    Copying a file in python

--- copy_file.py
+++
@@ -1,7 +0,0 @@
-# copyfile() = copies contents of a file
-#copy()= copyfile() + permission mode + destination can be a directory
-# copy2() copy() + copies metadata (file's creation and modificatio times)
-
-import shutil
-
-shutil.copyfile("copy.txt", 'new.txt')# source, dst
