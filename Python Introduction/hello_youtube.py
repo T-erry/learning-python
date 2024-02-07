c4bcf24e7325d39c5e334e44a268e3c06c709784
T-erry <terrywachuka10@gmail.com>
2024-02-07 15:49:29 +0300

    covered: modules in python

---
+++ Python Introduction/hello_youtube.py
@@ -0,0 +1,4 @@
+from messages import gretings,bye
+
+gretings()
+bye()
---
+++ Python Introduction/messages.py
@@ -0,0 +1,9 @@
+# module = a file containing python code. May contain funtions, classes, etc,
+# used with modular programming, which is to separate a program into parts
+# help("modules")
+def gretings():
+    print("Hello, Have a nice day!")
+
+
+def bye():
+     print("Have a wonderful time")
